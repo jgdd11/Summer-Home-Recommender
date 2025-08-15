e885743b4ba7db67360f1d77e75899b18c0640d6
jgdd11 <johngdion@hotmail.com>
2025-08-14 21:25:27 -0400

    Began working on main.py

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-import user-db.py
+import getpass
+import users
+import recommender
+
+def login():
+    username = input("Enter your username: ")
+    password = getpass.getpass("Enter your password: ")
+    return [username, password]
