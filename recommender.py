129e3b129fb9517f6f15326a41c76d01b8fb94cb
Annie Fa <fawanchun@gmail.com>
2025-08-17 19:43:48 -0400

    recommendation logic

---
+++ recommender.py
@@ -0,0 +1,6 @@
+def recommendation_logic(propety_list,user_req):
+    #function #1: assign properties a score 0 if 
+    # 1) location doesn't match, 
+    # 2) group size exceeds the number of people a property can hold
+    # 3) the property has unavailable dates
+    pass
