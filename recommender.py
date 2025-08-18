4ba9a872d25fb8e483ca8c75a0e6e9424e32f06c
Annie Fa <fawanchun@gmail.com>
2025-08-18 08:25:57 -0400

    Logic for recommending properties

--- recommender.py
+++ recommender.py
@@ -3,12 +3,25 @@ import pandas as pd
 def recommendation_logic(property_list,user_req):
     df = pd.DataFrame(property_list) #load property_list as dataframe
 
-    #function #1: assign location a score 1 if location matches, otherwise 0
+    #assign location a score 1 if location matches, otherwise 0
 
-    #function #2: assign group_size a score 1 if a property can hold the number of people, otherwise 0
+    #assign capacity a score 1 if a property can hold that group size, otherwise 0
     #group_size <= property capacity
 
-    #function #3: assign date a score 1 if the property is available during that period, otherwise 0
+    #assign availability a score 1 if the property is available during that period, otherwise 0
 
+    #assign price a score 1 if the price is at or below budget, otherwise 0
+    #price <= budget
 
+    #assign environment a score 1 if environment matches, otherwise 0
+
+    #assign feature a score 1 if feature matches, otherwise 0
+
+    #assign property scoe 0 if location, capacity or availability is 0
+
+    #calculate property score using weighted average
+
+    #rank property by property score
+
+    #display top N
     pass
