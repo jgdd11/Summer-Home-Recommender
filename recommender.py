485c0dbbe535246537645bac4145fadee17cb9d5
Annie Fa <fawanchun@gmail.com>
2025-08-18 21:31:40 -0400

    Recommendation logic

--- recommender.py
+++ recommender.py
@@ -1,12 +1,49 @@
 import pandas as pd
+import numpy as np
+from datetime import timedelta
 
 def recommendation_logic(property_list,user_req):
-    df = pd.DataFrame(property_list) #load property_list as dataframe
 
-    #kill properties that are 
-    # 1) in the wrong location, 
-    # 2) capacity is below group size
-    # 3) unavailable
+    #preferrably, dates need to load as date object, same for properties and user_req
+
+    df = pd.DataFrame(property_list) #load property_list as dataframe
+    print(df.head(10)) 
+    print(df.shape[0]) #print number of rows in the data frame, can be used to check if properties that don't match have been removed
+
+    #load user requirement
+    user_location = user_req["location"]
+    group_size = user_req["group_size"]
+    start_date = user_req["start_date"] 
+    end_date = user_req["end_date"]
+    budget_low = user_req["budget_low"]
+    budget_high = user_req["budget_high"]
+    user_features = user_req["features"]
+    user_environment = user_req["environment"]
+    user_tags = user_req["tags"]
+    budget_wt = user_req["budget_wt"]
+    enviro_wt = user_req["enviro_wt"]
+    feature_wt = user_req["feature_wt"]
+    tags_wt = user_req["tags_wt"]
+
+
+    #drop properties that don't match location or group size
+    df = df[df["location"] == user_location] 
+    df = df[df["capacity"] < group_size] 
+
+    
+    
+    #the following portion of the code doesn't work, need to debug
+    try:
+        drop_dates = set(pd.date_range(start_date, end_date))
+        df = df[~df["booked_dates"].apply(lambda lst: bool(set(lst) & drop_dates))]
+    except TypeError:
+        current_date = start_date
+        while current_date <= end_date:
+            df = df[~df["booked_dates"].apply(lambda lst: current_date in lst)]
+            current_date += timedelta(days=1)
+
+    print(df.shape[0]) #print number of rows in the data frame again, number should be less than the original row num, showing some rows have been dropped
+
 
     
 
